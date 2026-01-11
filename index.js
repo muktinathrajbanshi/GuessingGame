d7e716f8c0e88292d58979b70eeb10f3d9ebc385
unknown <muktinathrajbanshi786@gmail.com>
2026-01-11 20:39:55 +0545

    are wah haha

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const init = () => {
     // console.log(computerGuess);
     document.getElementById("newGameButton").display = "none";
     document.getElementById("gameArea").style.display = "none";
-
 };
+
+const startGame = () => {
+    document.getElementById("welcomeScreen").style.display = "none";
+    document.getElementById("gameArea").style.display = "block";
+}
+
+const easyMode = () => {
+    maxGuess = 10;
+    startGame();
+}
+
+const hardMode = () => {
+    maxGuess = 5;
+    startGame();
+}
 
